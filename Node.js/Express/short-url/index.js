7b79d14fa4916cd21f913073610c15b56a0a29cd
Narendra Saraf <Narendrasaraf91@outlook.com>
2026-06-22 18:24:01 +0530

    Short-Url login/signup

--- Node.js/Express/short-url/index.js
+++ Node.js/Express/short-url/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const { connectToMongoDB } = require("./connection");
+const { restrictToLoggedinUserOnly, checkAuth } = require('./middleware/auth');
 
 const urlRoute = require("./routes/url");
 const staticRoute = require("./routes/staticRouter");
-const URL = require("./models/url");
+const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8001;
@@ -13,33 +15,15 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
     console.log("Mongodb connected")
 );
 
-// Set view engine and location configuration
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-// Middlewares to handle incoming formats
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-// Routed paths
-app.use("/url", urlRoute);
-app.use("/", staticRoute);
-
-app.get("/url/:shortId", async (req, res) => {
-    const shortId = req.params.shortId;
-    const entry = await URL.findOneAndUpdate(
-        {
-            shortId,
-        },
-        {
-            $push: {
-                visitHistory: {
-                    timestamp: Date.now(),
-                },
-            },
-        }
-    );
-    res.redirect(entry.redirectURL);
-});
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/", checkAuth, staticRoute);
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
